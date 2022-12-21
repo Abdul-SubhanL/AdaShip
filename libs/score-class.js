67966f7857d7065aecabd8819ee74d7f85cf02a2
Abdul-SubhanL <AbdulSubhan.Latif@arup.com>
2022-12-21 12:23:23 +0000

    created score class

---
+++ libs/score-class.js
@@ -0,0 +1,17 @@
+class Score {
+  constructor() {
+    this.hits = 0;
+    this.misses = 0;
+    this.turn = true;
+  }
+
+  // method to hit oponents board at specified coordinates
+
+  // method to add/remove to score
+
+  // method to tally hits and misses
+
+  // method to see who's turn it is - one player is true, other one is false (like heads and tails)
+
+  // method to check if game is over
+}
